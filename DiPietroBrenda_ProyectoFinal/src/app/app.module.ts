d2796e6307e3b7767fa6fd1b492298c4a377c8a1
brendadipi <dipietrobrenda@gmail.com>
2023-04-28 18:12:37 -0300

    agregue cosas

--- DiPietroBrenda_ProyectoFinal/src/app/app.module.ts
+++ DiPietroBrenda_ProyectoFinal/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoArgProComponent } from './componentes/logo-arg-pro/logo-arg-pro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgProComponent
   ],
   imports: [
     BrowserModule,
